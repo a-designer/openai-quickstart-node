dd690133f9249989fa10954a7aae4f1344fb4440
a-designer <smagin@gmail.com>
2022-12-11 19:20:33 -0500

    Add translate to emoji page

---
+++ pages/api/generateemoji.js
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: generatePrompt(req.body.phrase),
+    temperature: 0.6,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+}
+
+function generatePrompt(phrase) {
+  const capitalizedPhrase =
+    phrase[0].toUpperCase() + phrase.slice(1).toLowerCase();
+  return `transform the phrase into emojis
+
+I love apples: ❤️🍎
+Dog walks in a park : 🐶🚶🏞️
+Party had great music : 🎉 🎼🎧️ ❤️
+${capitalizedPhrase} :`;
+}
---
+++ pages/emoji.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [phraseInput, setPhraseInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const response = await fetch("/api/generateemoji", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ phrase: phraseInput }),
+    });
+    const data = await response.json();
+    setResult(data.result);
+    setPhraseInput("");
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Translate to emoji</title>
+        <link rel="icon" href="/emoji.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <h2 className={styles.icon}>🔠😄</h2>
+        <h3>Translate to emoji</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="phrase"
+            placeholder="Enter a phrase"
+            value={phraseInput}
+            onChange={(e) => setPhraseInput(e.target.value)}
+          />
+          <input type="submit" value="Generate Emoji translation" />
+        </form>
+        <div className={styles.result}>{result}</div>
+      </main>
+    </div>
+  );
+}
